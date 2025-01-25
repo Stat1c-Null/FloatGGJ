8a01f87091cf00823157d58330a025df99102231
Mikita Slabysh <slabysh2015@gmail.com>
2025-01-24 19:48:11 -0500

    Basic Player Movement

---
+++ FloatGGJ/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 5.0f;
+    public float jump = 5.0f;
+    private Vector3 movementDirection;
+    public bool isGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+        
+        if((Input.GetButtonDown("Jump") || Input.GetAxis("Vertical") > 0) && isGrounded) {
+            isGrounded = false;
+            rb.velocity = new Vector3(rb.velocity.x, jump, rb.velocity.z);
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(movementDirection.x * speed, rb.velocity.y, rb.velocity.z);
+    }
+
+    public void OnCollisionEnter(Collision other) {
+        if(other.collider.tag == "Ground") {
+            isGrounded = true;
+        }
+    }
+}
