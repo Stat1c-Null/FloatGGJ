2e92018887e3ba378537904e4aa077a851226169
Danae Troupe <danaetroupe@gmail.com>
2025-01-26 03:52:24 -0500

    Added dialogue + increased walking speed based on scale

--- FloatGGJ/Assets/Scripts/TriggerDialogue.cs
+++ FloatGGJ/Assets/Scripts/TriggerDialogue.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TriggerDialogue : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI dialogueViewer;
+    bool triggered = false;
+    public string dialogue;
+
+    private void OnTriggerEnter(Collider collision)
     {
-        
+        if (!triggered && collision.CompareTag("Player"))
+        {
+            StartCoroutine(ExecuteDialogue());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ExecuteDialogue()
     {
-        
+        triggered = true;
+        dialogueViewer.text = dialogue;
+        yield return new WaitForSeconds(3);
+        dialogueViewer.text = "";
     }
 }
